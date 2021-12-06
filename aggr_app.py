53666e2ccadfe36a052319adc0eb40ba4a833ae7
nadzyah <nadzya.info@gmail.com>
2021-12-06 11:02:47 +0300

    bug fixes

--- aggr_app.py
+++ aggr_app.py
@@ -1,7 +1,4 @@
 """Log aggregator"""
-import sys
-sys.path.extend(["/home/nadzya/Apps/log-anomaly-detector/"])
-
 from aggregator.log_aggregator import Aggregator
 from anomaly_detector.config import Configuration
 
